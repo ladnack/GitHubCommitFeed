85aa1abd64aa6b6b27d45ca0f1cc14d72b2142ee
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2017-02-27 00:52:14 +0900

    Get user’s events

--- GitHubCommitFeed/ViewController.swift
+++ GitHubCommitFeed/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     let USERS = "/users/"
     
     let FOLLOWING = "/following"
+    let EVENTS = "/events"
+    
     
     var followingUser: [String : Int] = [:]
     
@@ -26,38 +28,72 @@ class ViewController: UIViewController {
         let userName = "shoheiyokoyama"
         getUserInfo(userName)
         
-        getUserInfo("Kyohei-Sakai")
+//        getUserInfo("Kyohei-Sakai")
         
     }
     
     // ユーザ情報を取得する
-    func  getUserInfo(_ userName: String) {
-//        Alamofire.request(HTTP_GitHub_API + USERS + userName, method: .get)
+    private func  getUserInfo(_ userName: String) {
+        
         Alamofire.request(HTTP_GitHub_API + USERS + userName + FOLLOWING, method: .get)
-            .responseJSON { response in
-                
-                // responseのresultプロパティのvalueプロパティをコンソールに出力
-//                print(response.result.value ?? "nil")
-                
-                guard let object = response.result.value else { return }
-                
-                let json = JSON(object)
-//                print(json)
-                json.forEach { (_, json) in
-                    let name = json["login"].stringValue
-                    self.followingUser[name] = json["id"].intValue
-//                    print("Name: \(name), Id: \(self.followingUser[name])")
-                    
-                }
-                
-                print("あなたのフォロー：\(self.followingUser.count)")
-                self.followingUser.forEach { key, value in
-                    print("Name: \(key), Id: \(value)")
-                }
+        .responseJSON { response in
+            
+            guard let jsonObject = response.result.value else { return }
+            
+            let json = JSON(jsonObject)
+            json.forEach { (_, json) in
+                let name = json["login"].stringValue
+                self.followingUser[name] = json["id"].intValue
+            }
+            
+            print("あなたのフォロー：\(self.followingUser.count)")
+            self.followingUser.forEach { key, value in
+                print("Name: \(key), Id: \(value)")
+            }
+            
+            self.getEvents(userName)
         }
         
     }
+    
+    // ユーザのアクティビティ情報を取得する
+    private func getEvents(_ userName: String) {
+        
+        Alamofire.request(HTTP_GitHub_API + USERS + userName + EVENTS, method:.get)
+        .responseJSON { response in
+            
+            guard let jsonObject = response.result.value else { return }
+            let json = JSON(jsonObject)
+            var events = 0
+            json.forEach { (_, json) in
+                let eventType = json["type"].stringValue
+                let createDate = json["created_at"].stringValue
+                
+                print("Type: \(eventType), Date: \(createDate)")
+                events += 1
+            }
+            print("イベント数： \(events)")
+        }
+        
+    }
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
